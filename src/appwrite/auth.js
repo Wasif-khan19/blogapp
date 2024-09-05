cec7fb4fcb4949d6081e68f3fdb1fded951b89db
Wasif <wk5599426@gmail.com>
2024-09-05 09:53:30 +0500

    edited

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import { Account, Client } from "appwrite";
+import { Account, Client, ID } from "appwrite";
 import conf from "../conf/conf";
 
 export class Authservice {
@@ -10,6 +10,32 @@ export class Authservice {
       .setEndpoint(conf.appwriteBackendEndpoint)
       .setProject(conf.appwriteProjectId);
     this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
