b05886bfc98fea5b9ea0165921ce47931d4d1f67
Wasif <wk5599426@gmail.com>
2024-09-07 23:32:11 +0500

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
   return (
     <>
-      <h1 className="text-5xl text-white font-bold">Muhammad Wasif</h1>
+      <h1 className="text-5xl text-white font-bold">..</h1>
     </>
   );
 }
