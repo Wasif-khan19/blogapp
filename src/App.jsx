909da08e2d50f9e252111cb6f7e771438751bb30
Wasif <wk5599426@gmail.com>
2024-09-10 03:43:55 +0500

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
   return (
     <>
-      <h1 className="text-5xl text-white font-bold">..</h1>
+      <h1 className="text-5xl text-white font-bold">..wasif</h1>
     </>
   );
 }
--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,6 +1,57 @@
-export class Service{
-    
+import { Client, Databases, Storage } from "appwrite";
+import conf from "../conf/conf";
+
+export class Service {
+  client = new Client();
+  bucket;
+  databases;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteBackendEndpoint)
+      .setProject(conf.appwriteProjectId);
+    this.bucket = new Storage(this.client);
+    this.databases = new Databases(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteCollectionId,
+        conf.appwriteDatabaseId,
+        slug,
+        {
+          title,
+          slug,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite error :: createPost :: error", error);
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteCollectionId,
+        conf.appwriteDatabaseId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite error :: updatePost :: error", error);
+    }
+  }
 }
 
-const service = new Service()
-export default service
+const service = new Service();
+export default service;
