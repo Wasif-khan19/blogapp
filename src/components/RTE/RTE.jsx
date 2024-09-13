5fac03e622f137c89beb9decb66d98f37af3aa7f
Wasif <wk5599426@gmail.com>
2024-09-13 10:12:04 +0500

    edited

---
+++ src/components/RTE/RTE.jsx
@@ -0,0 +1,29 @@
+import { Editor } from '@tinymce/tinymce-react'
+
+const Rte = () => {
+  return (
+    <div>
+        <Editor
+        initialValue='default'
+        init={
+            {
+                branding: false,
+                height: 500,
+                menubar: true,
+                plugins:[
+                    'advlist autolink lists link image charmap print preview anchor',
+                    'searchreplace visualblocks code fullscreen',
+                    'insertdatetime media table paste code help wordcount'
+                ],
+                toolbar:
+                    'undo redo | formatselect | bold italic backcolor | \
+                    alignleft aligncenter alignright alignjustify | \
+                    bullist numlist outdent indent | removeformat | help'
+            }
+        }
+        />
+    </div>
+  )
+}
+
+export default Rte
